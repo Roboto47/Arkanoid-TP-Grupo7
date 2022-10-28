f4cb03f3b211f1916c7af1fb2b6c76003e97f961
Ezequiel Alejandro Garcia <112414829+Garuyha@users.noreply.github.com>
2022-10-27 21:27:06 -0300

    modificaciones

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import Phaser from 'phaser';
 import Game from './componentes/Game.js';
+import { Menu } from './componentes/Menu.js';
 import { useEffect, useState } from 'react';
 import  { JuegoAcabado } from './componentes/GameOver.js'
 import { Ganaste } from './componentes/Victoria.js';
@@ -16,7 +17,7 @@ function App() {
             type: Phaser.AUTO,
             width:1300,                            // Ancho de la pantalla
             height:750,                            // Altura de la pantalla
-            scene:[Game,JuegoAcabado,Ganaste],     // Se guardan las escenas del juego.
+            scene:[Menu, Game,JuegoAcabado,Ganaste],     // Se guardan las escenas del juego.
             physics: {
                 default: 'arcade',
                 arcade: {                               // Se habilitan las fisicas
